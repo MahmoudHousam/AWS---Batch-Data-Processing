ba1c0f15044d49f4654971b9d48e00072163134b
MahmoudHousam <mahmoudhousam60@gmail.com>
2025-03-13 02:18:05 +0200

    edits

--- infrastructure/aws_cdk/setup_redshift_serverless_iam_role.py
+++ infrastructure/aws_cdk/setup_redshift_serverless_iam_role.py
@@ -1,6 +1,6 @@
 import boto3
 import json
-from ..policies.redshift_policy import redshift_policy
+from infrastructure.policies.redshift_policy import redshift_policy
 
 aws_region = "us-east-1"
 iam_client = boto3.client("iam", region_name=aws_region)
